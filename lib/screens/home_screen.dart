d2e15f8481d61b3989c811b26e8f303eab5c6b61
Ankit Mandar <ankit.mandar.1@gmail.com>
2025-08-14 13:16:50 +0545

    Used Shared_Preferences

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
-  void afunction() {
-    print('Buttom is pressed');
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  double balance = 0;
+
+  void addMoney() async {
+    setState(() {
+      balance = balance + 500;
+    });
+    print('Balance added');
+
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    await prefs.setDouble('balance', balance);
+  }
+
+  void loadBalance() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    setState(() {
+      balance = prefs.getDouble('balance') ?? 0;
+    });
   }
 
   @override
@@ -25,7 +48,14 @@ class HomeScreen extends StatelessWidget {
               child: Column(
                 spacing: 16,
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: [Text('Balance Part'), Text('0')],
+                children: [
+                  Text('Balance Part'),
+                  Text('$balance'),
+                  ElevatedButton(
+                    onPressed: loadBalance,
+                    child: Text('Add Money'),
+                  ),
+                ],
               ),
             ),
 
@@ -36,7 +66,7 @@ class HomeScreen extends StatelessWidget {
                   backgroundColor: Colors.red[700],
                   minimumSize: Size(double.infinity, double.infinity),
                 ),
-                onPressed: afunction,
+                onPressed: addMoney,
                 child: Text('Add Money'),
               ),
             ),
